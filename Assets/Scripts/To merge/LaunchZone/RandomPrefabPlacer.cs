697443436fa7476f845c978484e9ac9f2ea8d8a7
PavLovesDog <pavlovesdoge@gmail.com>
2023-06-15 09:46:26 +1000

    Fixed weird "Analytics Error"
    -fixed error
    
    - encapsulated random prefab script within namespace
    
    - created temp objects to test mechanic
    
    -note on line 95 of GameManager script to set the rrpScript, if needed

--- Assets/Scripts/To merge/LaunchZone/RandomPrefabPlacer.cs
+++ Assets/Scripts/To merge/LaunchZone/RandomPrefabPlacer.cs
@@ -2,45 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomPrefabPlacer : MonoBehaviour
+namespace a1Jam
 {
-    public GameObject launchZonePrefab;  // The prefab to be instantiated.
-    public int minCount = 5;   // Minimum number of prefabs to be placed.
-    public int maxCount = 10;  // Maximum number of prefabs to be placed.
-    public Vector3 center;     // Center of the area where prefabs will be placed.
-    public Vector3 size;       // Size of the area where prefabs will be placed.
-
-    public void PlaceRandomPrefabs()
+    public class RandomPrefabPlacer : MonoBehaviour
     {
-        // Delete existing prefabs.
-        GameObject[] existingPrefabs = GameObject.FindGameObjectsWithTag("LaunchZone");
-        foreach (GameObject obj in existingPrefabs)
+        public GameObject launchZonePrefab;  // The prefab to be instantiated.
+        public int minCount = 5;   // Minimum number of prefabs to be placed.
+        public int maxCount = 10;  // Maximum number of prefabs to be placed.
+        public Vector3 center;     // Center of the area where prefabs will be placed.
+        public Vector3 size;       // Size of the area where prefabs will be placed.
+    
+        public void PlaceRandomPrefabs()
         {
-            Destroy(obj);
+            // Delete existing prefabs.
+            GameObject[] existingPrefabs = GameObject.FindGameObjectsWithTag("LaunchZone");
+            foreach (GameObject obj in existingPrefabs)
+            {
+                Destroy(obj);
+            }
+    
+            // Place new random prefabs.
+            int count = Random.Range(minCount, maxCount + 1);
+    
+            for (int i = 0; i < count; i++)
+            {
+                Vector3 position = center + new Vector3(Random.Range(-size.x / 2, size.x / 2),
+                                                        Random.Range(-size.y / 2, size.y / 2),
+                                                        Random.Range(-size.z / 2, size.z / 2));
+    
+                Instantiate(launchZonePrefab, position, Quaternion.identity);
+            }
         }
-
-        // Place new random prefabs.
-        int count = Random.Range(minCount, maxCount + 1);
-
-        for (int i = 0; i < count; i++)
+    
+        // Start is called before the first frame update
+        void Start()
         {
-            Vector3 position = center + new Vector3(Random.Range(-size.x / 2, size.x / 2),
-                                                    Random.Range(-size.y / 2, size.y / 2),
-                                                    Random.Range(-size.z / 2, size.z / 2));
-
-            Instantiate(launchZonePrefab, position, Quaternion.identity);
+            PlaceRandomPrefabs();
         }
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        PlaceRandomPrefabs();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
     }
 }
